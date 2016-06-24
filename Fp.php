41da7d23adb1b9571ffc2b38d88a42219c6141a5
pickknow <sovsov@gmail.com>
2016-06-24 22:07:07 +0800

    2016-6-06-24

--- Fp.php
+++ Fp.php
@@ -1,5 +1,11 @@
 <?php
-
+function dump(){
+    $args = func_get_args();
+    array_map(function($item) {
+        echo '<br>';
+        var_dump($item);       
+    },$args);
+}
 function curry($callback) {
     $args = array_slice(func_get_args(),1);
     $refl = new \ReflectionFunction($callback);
@@ -15,12 +21,52 @@ function curryHandler($callback,$args,$length) {
 }
 
 function compose(){
-    $funcs = array_reverse(func_get_args());
-    return function($input) use ($funcs) {
-        return array_reduce($funcs,function($pre,$next) {
+    $args = array_reverse(func_get_args());
+    return function($input) use ($args) {
+        return array_reduce($args,function($pre,$next) {
             return $next($pre);
         },$input);
     };
 }
+//reduce($functin,$init,$data)
+function reduce(){
+    $func =  curry(function($fn,$init,$data){
+        return array_reduce($data,$fn,$init);
+    });
+    return call_user_func_array($func,func_get_args());
+}
 
+function map($callback){
+    return function($input) use ($callback) {
+        return array_map($callback,$input);
+    };
+}
 
+function filter($callback) {
+    return function($input) use ($callback) {
+        return array_filter($input,$callback);
+    };
+}
+
+function chain($data){
+    return new Chain($data);
+}
+
+class Chain{
+    private $value;
+    function __construct($value){
+        $this->value = $value;
+    }
+
+    function map($callback) {
+        $this->value = array_map($callback,$this->value);
+        return $this;
+    }
+    function reduce($callback,$int) {
+        $this->value = array_reduce($this->value,$callback,$init);
+        return $this;
+    }
+    function value(){
+        return $this->value;
+    }
+}
--- index.php
+++ index.php
@@ -12,8 +12,28 @@ $add2 = curry($add,2);
 
 $add3 = compose($add1,$add2);
 
-var_dump($add3(3));
+// var_dump($add3(3));
 
 $add4 = compose($add1,$add3);
 
-var_dump($add4(4));
+// var_dump($add4(4));
+
+//reduce
+$fn = function($pre,$next) {
+    return $pre + $next;
+};
+
+$data = [1,2,3,4,5];
+
+$reduce = reduce($fn,0);
+dump($reduce($data));
+
+$reduce = compose($add1,$add2,reduce($fn,10));
+dump($reduce($data));
+
+$chain = chain($data)->map(function($item){
+   return ++$item;
+})->reduce($fn,0)->value();
+dump($chain);
+var_dump('dddd',$data);
+dump('dddd',$data);
